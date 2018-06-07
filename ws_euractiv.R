98fe221a63f7a37629ec879ddcba138e0bd46278
Carlos Carbajal <carbajal.carlos@hotmail.com>
2018-06-07 18:35:41 +0100

    Addition: Partial entry cleaning and project closing

--- ws_euractiv.R
+++ ws_euractiv.R
@@ -1,13 +1,12 @@
 # ----- Initialization -----
 # Loading required libraries
 require(rvest)
-
 # Creation of objects required 
-euractiv.dataset <- data.frame()
+euractiv <- data.frame()
 end <- FALSE
 i <- 1
 
-# Webscraping data
+# ----- Webscraping data -----
 while (end == FALSE) {
   url <- paste(c("https://www.euractiv.com/page/", i, "/?s=democracy+eu&year=2018&orderby=post_date&order=ASC"), collapse = "")
   pagina <- try(read_html(url))
@@ -46,33 +45,65 @@ while (end == FALSE) {
   subset <- cbind(headline, content.type, topic, date, summary, hyperlink)
   subset <- as.data.frame(subset, stringsAsFactors = FALSE)
   
-  euractiv.dataset <- rbind(euractiv.dataset, subset)
+  euractiv <- rbind(euractiv, subset)
 
   i <- i+1
 }
 closeAllConnections()
-
 # Removing entries without hyperlink
-euractiv.dataset <- euractiv.dataset[!is.na(euractiv.dataset$hyperlink), ]
+euractiv <- euractiv[!is.na(euractiv$hyperlink), ]
 
-# Extracting full text
-euractiv.dataset$source <- NA
-euractiv.dataset$full.text <- NA
-
+# ----- individual extraction -----
+euractiv$source <- NA
+euractiv$full.text <- NA
 # Extracting especific data
-for (i in 1:nrow(euractiv.dataset)) {
-  url <- euractiv.dataset$hyperlink[i]
+for (i in 1:nrow(euractiv)) {
+  url <- euractiv$hyperlink[i]
   pagina <- try(read_html(url))
   # source
   node <- ".ea-article-header .ea-article-meta"
-  euractiv.dataset$source[i] <- tryCatch(pagina %>% html_nodes(node) %>% html_text(),
+  euractiv$source[i] <- tryCatch(pagina %>% html_nodes(node) %>% html_text(),
                                             warning = function(w) {print("Fail in 1"); return(NA)})
   # full.text
   node <- ".ea-article-body-content"
-  euractiv.dataset$full.text[i] <- tryCatch(pagina %>% html_nodes(node) %>% html_text(),
+  euractiv$full.text[i] <- tryCatch(pagina %>% html_nodes(node) %>% html_text(),
                                          warning = function(w) {print("Fail in 1"); return(NA)})
 }
 
+# ----- Cleaning entries -----
+### Date
+# Removing dobuble spaces
+index <- grep(pattern = "[[:space:]]+", x = euractiv$date)
+euractiv$date[index] <-  trimws(gsub(pattern = "[[:space:]]+", replacement = " ", x = euractiv$date[index]), which = "both")
+
+### Source
+# Removing dobuble spaces
+index <- grep(pattern = "[[:space:]]+", x = euractiv$source)
+euractiv$source[index] <-  trimws(gsub(pattern = "[[:space:]]+", replacement = " ", x = euractiv$source[index]), which = "both")
+
+### Full text
+# Removing dobuble spaces
+euractiv$clean.text <- euractiv$full.text
+index <- grep(pattern = "[[:space:]]+", x = euractiv$clean.text)
+euractiv$clean.text[index] <-  trimws(gsub(pattern = "[[:space:]]+", replacement = " ", x = euractiv$clean.text[index]), which = "both")
+
+# ----- Closing project -----
+# Removing objects
+rm(pagina)
+rm(subset)
+rm(content.type)
+rm(date)
+rm(end)
+rm(headline)
+rm(hyperlink)
+rm(i)
+rm(index)
+rm(node)
+rm(summary)
+rm(test)
+rm(topic)
+rm(url)
+
 # Storing results
-save(file = "output/1_euractiv.Rdata", list = "euractiv.dataset")
-write.csv(x = euractiv.dataset, file = "output/euractiv.csv")
+save(file = "output/1_euractiv.Rdata", list = "euractiv")
+write.csv(x = euractiv, file = "output/euractiv.csv")
