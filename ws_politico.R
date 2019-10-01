d08bed3018cb185d86e0ffb5fd0f1eab638d6d14
Carlos Carbajal <carbajal.carlos@hotmail.com>
2019-10-01 20:36:22 +0100

    Fine tuning of the web scraping code.

--- ws_politico.R
+++ ws_politico.R
@@ -1,21 +1,28 @@
-  # ----- Initialization -----
+# ===== 0_Initialization =====
 # Loading required libraries
 require(rvest)
 # Creation of objects required 
 politico <- data.frame()
 
-# ----- Webscraping data -----
-# Initial search
-url <- paste(c("https://www.politico.eu/page/1/?s=EU&sp%5Bf%5D=01%2F01%2F2014&sp%5Bt%5D=12%2F31%2F2018&orderby=date&order=asc"), collapse = "")
+# ===== 1_Webscraping headlines =====
+# Extracting number of pages
+year_sta <- 2014 # From January 01
+year_end <- 2014 # Until December 31.
+
+url <- paste(c("https://www.politico.eu/page/1/?s=EU&sp%5Bf%5D=01%2F01%2F", 
+               year_sta, "&sp%5Bt%5D=12%2F31%2F", year_end, 
+               "&orderby=date&order=asc"), collapse = "")
 pagina <- try(read_html(url))
-# Identifiying number of pages
 node <- "li:nth-child(3) .page-numbers"
 num_pag <- as.integer(tryCatch(pagina %>% html_nodes(node) %>% html_text(),
                                warning = function(w) {print("Fail in 1"); return(NA)}))
 
+# Headlines and summary extracting sequence
 for (i in 1:num_pag) {
   # website access
-  url <- paste(c("https://www.politico.eu/page/", i, "/?s=EU&sp%5Bf%5D=01%2F01%2F2014&sp%5Bt%5D=12%2F31%2F2014&orderby=date&order=asc"), collapse = "")
+  url <- paste(c("https://www.politico.eu/page/", i, "/?s=EU&sp%5Bf%5D=01%2F01%2F", 
+                 year_sta, "&sp%5Bt%5D=12%2F31%2F", year_end, 
+                 "&orderby=date&order=asc"), collapse = "")
   pagina <- try(read_html(url))
   
   # headline
@@ -59,95 +66,116 @@ politico <- politico[!is.na(politico$hyperlink), ]
 # Removing duplicated entries
 politico <- politico[!duplicated(politico$headline),]
 
-# filtering
-# Basically, I need all articles containing EU and:
-# democra* or reform* or institut* or future 
-table(euractiv$content.type)
-index <- grep(pattern = 'video', x = euractiv$content.type, ignore.case = TRUE)
-euractiv$hyperlink[index]
+# ===== 2_Filtering =====
 
+#===> This section requires to filter the articles that you would like to extract
+#===> From the dataset available at the moment, you can search in the headline or
+#===> the summary for the keywords that you mentioned before.
 
-table(euractiv$topic)
-euractiv <- subset(x = euractiv, subset = topic != 'Transport')
-euractiv <- subset(x = euractiv, subset = topic != 'Health')
-euractiv <- subset(x = euractiv, subset = topic != 'Agrifood')
+# Your text message:  Basically, I need all articles containing EU and 
+#                     democra* or reform* or institut* or future 
 
+# This is what I did with euractiv website:
 
-# ----- individual extraction -----
-euractiv$source <- NA
-euractiv$full.text <- NA
-euractiv$promoted <- NA
+# table(euractiv$content.type)
+# index <- grep(pattern = 'video', x = euractiv$content.type, ignore.case = TRUE)
+# euractiv$hyperlink[index]
+# 
+# 
+# table(euractiv$topic)
+# euractiv <- subset(x = euractiv, subset = topic != 'Transport')
+# euractiv <- subset(x = euractiv, subset = topic != 'Health')
+# euractiv <- subset(x = euractiv, subset = topic != 'Agrifood')
+
+# ===== 3_Individual entries extraction =====
+politico$source <- NA
+politico$author <- NA
+politico$full.text <- NA
+politico$categories <- NA
+
 # Extracting especific data
-for (i in 1:nrow(euractiv)) {
-  url <- euractiv$hyperlink[i]
+for (i in 1:nrow(politico)) {
+  url <- politico$hyperlink[i]
   pagina <- try(read_html(url))
-  # source
-  node <- ".ea-article-header .ea-article-meta"
-  euractiv$source[i] <- tryCatch(pagina %>% html_nodes(node) %>% html_text(),
+  # Source
+  node <- "p.byline"
+  ws <- tryCatch(pagina %>% html_nodes(node) %>% html_text(),
+                                 warning = function(w) {print("Fail in 1"); return(NA)})
+  politico$source[i] <- tail(x = ws, n = 1)
+  
+  # Author
+  node <- ".credits-author .fn"
+  ws <- tryCatch(pagina %>% html_nodes(node) %>% html_text(),
+                                warning = function(w) {print("Fail in 1"); return(NA)})
+  if (length(ws) > 0) {
+    politico$author[i] <- tail(x = ws, n = 1)
+  }
+  
+  # Full-text
+  node <- ".has-sidebar > p"
+  ws <- tryCatch(pagina %>% html_nodes(node) %>% html_text(),
+                                    warning = function(w) {print("Fail in 1"); return(NA)})
+  politico$full.text[i] <- paste(ws, collapse  = " ")
+  
+  # Categories
+  node <- ".categories-list a"
+  ws <- tryCatch(pagina %>% html_nodes(node) %>% html_text(),
                                             warning = function(w) {print("Fail in 1"); return(NA)})
-  # full.text
-  node <- ".ea-article-body-content"
-  euractiv$full.text[i] <- tryCatch(pagina %>% html_nodes(node) %>% html_text(),
-                                         warning = function(w) {print("Fail in 1"); return(NA)})
-  # Is promoted
-  node <- '#main_container b'
-  temp <- tryCatch(pagina %>% html_nodes(node) %>% html_text(),
-                                   warning = function(w) {print("Fail in 1"); return(NA)})
-  if (length(temp) != 0) {
-    euractiv$promoted[i] <- temp[1]
+  politico$categories[i] <- paste(ws, collapse  = ";   ")
+ 
+   # Printing progress
+  if (i == 1) {
+    print(paste("Number of entries:", nrow(politico)))
+  }
+  if (i%%10 == 0) {
+    print(paste(c("Progress: ", i, "/", nrow(politico)), collapse = ""))
   }
 }
 
-# ----- Cleaning entries -----
+# Security feature
+closeAllConnections()
+
+# ===== 4_Cleaning entries =====
 ### Date
 # Removing dobuble spaces
-index <- grep(pattern = "[[:space:]]+", x = euractiv$date)
-euractiv$date[index] <-  trimws(gsub(pattern = "[[:space:]]+", replacement = " ", x = euractiv$date[index]), which = "both")
-
-### Source
-# Removing dobuble spaces
-index <- grep(pattern = "[[:space:]]+", x = euractiv$source)
-euractiv$source[index] <-  trimws(gsub(pattern = "[[:space:]]+", replacement = " ", x = euractiv$source[index]), which = "both")
+index <- grep(pattern = "[[:space:]]+", x = politico$date)
+politico$date[index] <-  trimws(gsub(pattern = "[[:space:]]+", replacement = " ", x = politico$date[index]), which = "both")
 
 ### Full text
 # Removing dobuble spaces
-euractiv$clean.text <- euractiv$full.text
-index <- grep(pattern = "[[:space:]]+", x = euractiv$clean.text)
-euractiv$clean.text[index] <-  trimws(gsub(pattern = "[[:space:]]+", replacement = " ", x = euractiv$clean.text[index]), which = "both")
+politico$clean.text <- politico$full.text
+index <- grep(pattern = "[[:space:]]+", x = politico$clean.text)
+politico$clean.text[index] <-  trimws(gsub(pattern = "[[:space:]]+", replacement = " ", x = politico$clean.text[index]), which = "both")
 
-# Promoted content
-index <- grep(pattern = 'promoted', x = euractiv$promoted, ignore.case = TRUE)
-euractiv$promoted <- FALSE
-euractiv$promoted[index] <- TRUE
+# Generating entries ID
+politico$id <- 1:nrow(politico)
 
-# test
-
-# Here is a change
-
-# ----- Closing project -----
+# ===== 5_Closing project =====
 # Removing objects
 rm(pagina)
 rm(subset)
-rm(content.type)
 rm(date)
-rm(end)
 rm(headline)
 rm(hyperlink)
 rm(i)
 rm(index)
 rm(node)
+rm(num_pag)
 rm(summary)
 rm(test)
-rm(topic)
 rm(url)
+rm(ws)
 
-# Storing results
-save(file = "output/1_euractiv_14.Rdata", list = "euractiv")
-write.csv(x = euractiv, file = "output/euractiv_14.csv")
+# Storing results change the name accordingly
+name <- paste(c("output/politico_", year_sta, "-", year_end, ".Rdata"), 
+              collapse = "")
+save(file = name, list = "politico")
+name <- paste(c("output/politico_", year_sta, "-", year_end, ".csv"), 
+              collapse = "")
+write.csv(x = politico, file = name)
 
-euractiv$id <- 1:nrow(euractiv)
-
-for (u in 1:nrow(euractiv)) {
-  name <- paste(c('output/ind/art_', euractiv$id[u], '.txt'), collapse = '')
-  write(x = euractiv$full.text[u], file = name)
+#Extracting entries in individual text files.
+for (u in 1:nrow(politico)) {
+  name <- paste(c('output/ind_pol/art_', politico$id[u], '.txt'), collapse = '')
+  write(x = politico$full.text[u], file = name)
 }
